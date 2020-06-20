85f64b6bfebf9d837622180a7ba729042fff7ba0
fh <1912339826@qq.com>
2020-06-20 09:42:11 +0800

    资讯api的更改

--- MYDome/src/api/api2.js
+++ MYDome/src/api/api2.js
@@ -11,7 +11,8 @@ if (process.env.NODE_ENV == "development") {
 } else if (process.env.NODE_ENV == "production") {
   // 生产
   var BASE_URLA = "http://47.98.169.218:8083";
-  var information = "http://47.98.169.218:8090"
+  // var information = "http://47.98.169.218:8090"
+  var information = "http://sign.xn--fjq0sg8h2zkivvwsonptcv2b.com/web/api:8090"
 }
 window.information = information;
 window.BASE_URLA = BASE_URLA;
@@ -22,23 +23,23 @@ export default {
   // 人脸补录
   // updatePersonImg: `POST ${BASE_URLA}/faceplat/facePersonImg/updatePersonImg.do`
   // 获取类别列表
-  catList:`${BASE_URLA}/textbook/catList`,
+  catList: `${BASE_URLA}/textbook/catList`,
   // 获取列表 
-  list:`${BASE_URLA}/textbook/list`,
+  list: `${BASE_URLA}/textbook/list`,
   // 教材详情 
-  info:`${BASE_URLA}/textbook/info`,
+  info: `${BASE_URLA}/textbook/info`,
   // 获取考试须知 
-  getNotes:`${BASE_URLA}/exam/notes/getNotes`,
+  getNotes: `${BASE_URLA}/exam/notes/getNotes`,
   // 获取考试流程 
-  getProcess:`${BASE_URLA}/exam/process/getProcess`,
+  getProcess: `${BASE_URLA}/exam/process/getProcess`,
   // 资讯类别列表
-  informationCategory:`${information}/information/category/list`,
+  informationCategory: `${information}/information/category/list`,
   // 资讯列表
-  informationList:`${information}/information/list`,
+  informationList: `${information}/information/list`,
   // 资讯详情 
-  informationInfo:`${information}/information/info`,
+  informationInfo: `${information}/information/info`,
   // 获取考试流程详情
-  getProcessInfo:`${BASE_URLA}/exam/process/getProcessInfo`,
+  getProcessInfo: `${BASE_URLA}/exam/process/getProcessInfo`,
   // 联系我们 
-  contact:`${BASE_URLA}/contact/us/search`,
+  contact: `${BASE_URLA}/contact/us/search`,
 };
