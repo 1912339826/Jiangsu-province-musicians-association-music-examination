200ab1be5932b403e6140067cfeee0facdbebd04
username@email.com <1912339826@qq.com>
2020-06-22 19:24:14 +0800

    请求连接

--- MYDome/src/api/api2.js
+++ MYDome/src/api/api2.js
@@ -1,16 +1,16 @@
 if (process.env.NODE_ENV == "development") {
   // 开发
   window.development = false
-  // var BASE_URLA = "http://47.98.169.218:8083";
+  var BASE_URLA = "http://47.98.169.218:8083";
   var information = "http://47.98.169.218:8090"
 } else if (process.env.NODE_ENV == "debug") {
   // debug
-  // var BASE_URLA = "http://47.98.169.218:8083";
+  var BASE_URLA = "http://47.98.169.218:8083";
   var information = "http://47.98.169.218:8090"
 } else if (process.env.NODE_ENV == "production") {
   // 生产
   window.development = true
-  // var BASE_URLA = "http://47.98.169.218:8083";
+  var BASE_URLA = "http://sign.xn--fjq0sg8h2zkivvwsonptcv2b.com/api";
   var information = "http://sign.xn--fjq0sg8h2zkivvwsonptcv2b.com/web/api"
 }
 window.information = information;
@@ -22,15 +22,15 @@ export default {
   // 人脸补录
   // updatePersonImg: `POST ${BASE_URLA}/faceplat/facePersonImg/updatePersonImg.do`
   // 获取类别列表
-  catList: `${information}/textbook/catList`,
+  catList: `${BASE_URLA}/textbook/catList`,
   // 获取列表 
-  list: `${information}/textbook/list`,
+  list: `${BASE_URLA}/textbook/list`,
   // 教材详情 
-  info: `${information}/textbook/info`,
+  info: `${BASE_URLA}/textbook/info`,
   // 获取考试须知 
-  getNotes: `${information}/exam/notes/getNotes`,
+  getNotes: `${BASE_URLA}/exam/notes/getNotes`,
   // 获取考试流程 
-  getProcess: `${information}/exam/process/getProcess`,
+  getProcess: `${BASE_URLA}/exam/process/getProcess`,
   // 资讯类别列表
   informationCategory: `${information}/information/category/list`,
   // 资讯列表
@@ -38,7 +38,7 @@ export default {
   // 资讯详情 
   informationInfo: `${information}/information/info`,
   // 获取考试流程详情
-  getProcessInfo: `${information}/exam/process/getProcessInfo`,
+  getProcessInfo: `${BASE_URLA}/exam/process/getProcessInfo`,
   // 联系我们 
-  contact: `${information}/contact/us/search`,
+  contact: `${BASE_URLA}/contact/us/search`,
 };
